36aece01f988d970943bdcc7f9b85183a2dff72d
hikidisn <masonbgalloway@outlook.com>
2026-07-27 16:59:11 -0400

    updated a small bug in terminal.c

--- terminal.c
+++ terminal.c
@@ -13,26 +13,6 @@ void newline() {
 	old = row;
 }
 
-
-
-enum vga_color {
-	black = 0,
-	blue = 1,
-	green = 2,
-	cyan = 3,
-	red = 4,
-	purple = 5,
-	brown = 6,
-	gray = 7,
-	dark_gray = 8,
-	light_blue = 9,
-	light_green = 10,
-	light_cyan = 11,
-	light_red = 12,
-	light_purple = 13,
-	yellow = 14,
-	white = 15
-};
 int color = green;
 
 void tracker(char x, int  y) {
